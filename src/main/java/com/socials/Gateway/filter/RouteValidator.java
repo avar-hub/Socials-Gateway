bc31ae70e17fae5a4fe9f5ed68be7db53a5df1b3
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 <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>
2024-04-11 02:58:11 +0530

    Refresh token api

--- src/main/java/com/socials/Gateway/filter/RouteValidator.java
+++ src/main/java/com/socials/Gateway/filter/RouteValidator.java
@@ -16,6 +16,7 @@ public class RouteValidator {
             "auth/verify-account",
             "/auth/forgotPass",
             "/auth/resetPass",
+            "/auth/refreshToken",
             "eureka"
     );
 
